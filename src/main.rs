549c75c76fe3d4dbb97fe9ab5facfc6bbf6b6b28
chai2010 <chaishushan@gmail.com>
2022-02-11 22:30:25 +0800

    删除垃圾

--- src/main.rs
+++ src/main.rs
@@ -30,39 +30,4 @@ define i32 @main() {{
 		.arg("a.out.ll")
 		.output()
 		.unwrap();
-
-
-	//, , , 
-
-    //let output = format!(tmpl.as_ref(), code);
-    //println!(output);
 }
-
-/*
-use std::process::Command;
-
-    Command::new("sh")
-            .arg("-c")
-            .arg("echo hello")
-            .output()
-            .expect("failed to execute process")
-
-*/
-
-//define i32 @main() {
-//    ret i32 %v
-//}
-//";
-/*
-func compile(code string) {
-    output := fmt.Sprintf(tmpl, code)
-    os.WriteFile("a.out.ll", []byte(output), 0666)
-    exec.Command("clang", "-Wno-override-module", "-o", "a.out", "a.out.ll").Run()
-}
-
-const tmpl = `
-define i32 @main() {
-    ret i32 %v
-}
-`
-*/
