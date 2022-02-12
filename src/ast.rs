8b41feabbdecb6823527a4acdffba188cd6c1ddf
chai2010 <chaishushan@gmail.com>
2022-02-12 23:00:36 +0800

    ch2-2 done

---
+++ src/ast.rs
@@ -0,0 +1,5 @@
+pub struct ExprNode<'a> {
+    pub value: &'a str, // +, -, *, /, 123
+    pub left: Option<Box<ExprNode<'a>>>,
+    pub right: Option<Box<ExprNode<'a>>>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,47 @@
 // rustup doc
 
-// echo 1+3-2 | cargo run
-// ./a.out
-// echo $?
+#![allow(unused)]
+
+mod ast;
+mod compiler;
 
 fn main() {
-    let mut buffer = String::new();
-    std::io::stdin().read_line(&mut buffer).unwrap();
+    // 1+2*(3+4)
+    let node = crate::ast::ExprNode {
+        value: "+",
+        left: Some(Box::new(crate::ast::ExprNode {
+            value: "1",
+            left: None,
+            right: None,
+        })),
+        right: Some(Box::new(crate::ast::ExprNode {
+            value: "*",
+            left: Some(Box::new(crate::ast::ExprNode {
+                value: "2",
+                left: None,
+                right: None,
+            })),
+            right: Some(Box::new(crate::ast::ExprNode {
+                value: "+",
+                left: Some(Box::new(crate::ast::ExprNode {
+                    value: "3",
+                    left: None,
+                    right: None,
+                })),
+                right: Some(Box::new(crate::ast::ExprNode {
+                    value: "4",
+                    left: None,
+                    right: None,
+                })),
+            })),
+        })),
+    };
 
-    println!("{}", run(buffer.as_ref()));
+    println!("{}", run(&node));
 }
 
-fn run(code: &str) -> i32 {
-    compile(code);
+fn run(node: &crate::ast::ExprNode) -> i32 {
+    compile(node);
 
     let status = std::process::Command::new("./a.out").status().unwrap();
 
@@ -22,9 +51,9 @@ fn run(code: &str) -> i32 {
     }
 }
 
-fn compile(code: &str) {
-    let tokens = parse_tokens(code);
-    let output = gen_asm(&tokens);
+fn compile(node: &crate::ast::ExprNode) {
+    let mut c = crate::compiler::Compiler::new();
+    let output = c.gen_llir(node);
 
     std::fs::write("a.out.ll", output).unwrap();
 
@@ -35,80 +64,4 @@ fn compile(code: &str) {
         .arg("a.out.ll")
         .output()
         .unwrap();
-}
-
-fn gen_asm(tokens: &[&str]) -> String {
-    let mut result = String::new();
-
-    result.push_str("define i32 @main() {\n");
-
-    let mut idx = 0;
-    for (i, tok) in tokens.iter().enumerate() {
-        if i == 0 {
-            result.push_str(&format!("\t%t{} = add i32 0, {}\n", idx, tok));
-            continue;
-        }
-        match tok {
-            &"+" => {
-                idx = idx + 1;
-                result.push_str(&format!(
-                    "\t%t{} = add i32 %t{}, {}\n",
-                    idx,
-                    idx - 1,
-                    tokens[i + 1]
-                ));
-            }
-            &"-" => {
-                idx = idx + 1;
-                result.push_str(&format!(
-                    "\t%t{} = sub i32 %t{}, {}\n",
-                    idx,
-                    idx - 1,
-                    tokens[i + 1]
-                ));
-            }
-            _ => {}
-        }
-    }
-
-    result.push_str(&format!("\tret i32 %t{}\n", idx));
-    result.push_str("}\n");
-
-    result
-}
-
-fn parse_tokens(code: &str) -> Vec<&str> {
-    let mut tokens = Vec::new();
-    let mut pos = 0;
-
-    loop {
-        if let Some(i) = code[pos..].find('+') {
-            tokens.push(&code[pos..][..i]);
-            tokens.push(&code[pos..][i..][..1]);
-            pos = pos + i + 1;
-            continue;
-        }
-        if let Some(i) = code[pos..].find('-') {
-            tokens.push(&code[pos..][..i]);
-            tokens.push(&code[pos..][i..][..1]);
-            pos = pos + i + 1;
-            continue;
-        }
-
-        tokens.push(&code[pos..]);
-        return tokens;
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn run_works() {
-        assert_eq!(run("1"), 1);
-        assert_eq!(run("1+1"), 2);
-        assert_eq!(run("1 + 3 - 2"), 2);
-        assert_eq!(run("1+2+3+4"), 10);
-    }
 }
